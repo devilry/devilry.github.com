46e2afa0de1022a8f8f892b8ba386ebf2f066827
Espen A. Kristiansen <post@espenak.net>
2011-06-05 15:30:04 +0200

    Work on showcase

--- showcase/create-showcase.py
+++ showcase/create-showcase.py
@@ -1,28 +1,40 @@
 from sys import argv
 from glob import glob
 from os import sep
-from os.path import splitext, dirname, abspath, join
+from os.path import splitext, dirname, abspath, join, split, exists
 
 if len(argv) != 2:
     raise SystemExit("usage: %s <dirname>" % argv[0])
 
+def readtpl(name):
+    return open(join(this_dir, '%s.html' % name)).read()
+
+def path_to_urlpath(path):
+    return '/'.join(split(path))
+
 showcase_dir = argv[1]
 this_dir = abspath(dirname(__file__))
-template = open(join(this_dir, 'tpl.html')).read()
-singleimagetpl = open(join(this_dir, 'singleimagetpl.html')).read()
 outfile = '%(showcase_dir)s.html' % vars()
 single_image_ext = 'single.png'
-print this_dir
+
+template = readtpl('tpl')
+singleimagetpl = readtpl('singleimagetpl')
+onlytexttpl = readtpl('onlytext')
 
 parts = []
 for filename in glob('%(showcase_dir)s%(sep)s*.html' % vars()):
     name = splitext(filename)[0]
-    single_image_src = "%(showcase_dir)s/%(filename)s.%(single_image_ext)s" % vars()
+    singleimage_path = "%(name)s.%(single_image_ext)s" % vars()
+    if exists(singleimage_path):
+        singleimage_src = path_to_urlpath(singleimage_path)
+        tpl = singleimagetpl
+    else:
+        tpl = onlytexttpl
+
     text = open(filename).read()
-    parts.append(singleimagetpl % dict(
-        src=single_image_src,
-        text=text))
+    parts.append(tpl % dict(vars()))
 
 main = '\n\n'.join(parts)
 out = template % dict(main=main)
 open(outfile, 'wb').write(out)
+print 'Output written to %s.' % outfile
