27c1e6e9bd5f6c71608fa80efe4bb7b768757c0d
Espen A. Kristiansen <post@espenak.net>
2011-06-05 21:23:09 +0200

    Initial carousel

--- showcase/create-showcase.py
+++ showcase/create-showcase.py
@@ -1,5 +1,6 @@
 from sys import argv
 from os.path import dirname, abspath, join, split, exists
+from os import listdir
 
 if len(argv) != 2:
     raise SystemExit("usage: %s <dirname>" % argv[0])
@@ -12,31 +13,46 @@ def path_to_urlpath(path):
 
 showcase_dir = argv[1].replace('/', '')
 this_dir = abspath(dirname(__file__))
+outdir = dirname(this_dir)
 outfile = '%(showcase_dir)s.html' % vars()
 single_image_ext = 'single.png'
 
 template = readtpl('tpl')
 singleimagetpl = readtpl('singleimagetpl')
+bigimagetpl = readtpl('bigimagetpl')
 onlytexttpl = readtpl('onlytext')
 
 parts = []
 partnames = [x.strip() for x in open(join(showcase_dir, 'index.txt')).readlines()]
 print partnames
 pervious_img_floatleft = True # did the previous part have its image on the left?
-for partname in partnames:
-    print "Parsing", partname
-    singleimage_name = "%(partname)s.%(single_image_ext)s" % vars()
-    singleimage_path = join(showcase_dir, singleimage_name)
-    hasimage = False
-    if exists(singleimage_path):
-        hasimage = True
-        singleimage_src = '%s/%s' % (showcase_dir, singleimage_name)
-        tpl = singleimagetpl
+for part in partnames:
+    p = [x.strip() for x in part.split(': ')]
+    partname = p[0]
+    if len(p) > 1:
+        imagesize, multi = p[1].split(',')
     else:
-        tpl = onlytexttpl
+        imagesize, multi = '', ''
+    print "Parsing", partname
+
+    tpl = onlytexttpl
+    if imagesize:
+        if imagesize == 'small':
+            singleimage_name = "%(partname)s.%(single_image_ext)s" % vars()
+            singleimage_src = '%s/%s' % (showcase_dir, singleimage_name)
+            tpl = singleimagetpl
+        elif imagesize == 'big' and multi == 'multi':
+            imagenames = [item for item in listdir(showcase_dir) \
+                    if item.startswith('%s.multi.' % partname)]
+            imagenames.sort()
+            images = '\n'.join([
+                '<li><img src="%s/%s"/></li>' % (showcase_dir, i)\
+                for i in imagenames])
+            tpl = bigimagetpl
+            
 
     extraclasses = ''
-    if hasimage:
+    if imagesize == 'small':
         if pervious_img_floatleft:
             pervious_img_floatleft = False
             extraclasses = 'right'
