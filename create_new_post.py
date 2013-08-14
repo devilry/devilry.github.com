f0c76bff7b0ad739e3549970a266fd9f337b961c
Tor Ivar Johansen <toij@ifi.uio.no>
2013-08-14 02:04:05 +0200

    * Added enabling of comments

--- create_new_post.py
+++ create_new_post.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-def create_new_post_as_now(title):
+def create_new_post_as_now(title, comments):
     date = datetime.now()
     title_in_filename = title.replace(' ','-').lower() 
     filename = date.strftime("%Y-%m-%d-{0}.markdown".format(title_in_filename))
@@ -11,13 +11,14 @@ def create_new_post_as_now(title):
     fileheader = """---
 layout: post
 title: {0}
----""".format(title)
+comments: {1}
+---\n""".format(title, comments)
     file.write(fileheader)
     file.close()
     print "Done! created new file: <{0}> with appended YAML front matter".format(filename) 
 
 if __name__ == "__main__":
     title = raw_input("Name your new post: ")
-    print title
-    create_new_post_as_now(title)
+    comments = raw_input("Set true/false on comments: ")
+    create_new_post_as_now(title, comments)
     
